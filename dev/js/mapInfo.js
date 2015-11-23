51a5e991ecfcd328f15adad704a9cf208359860c
Patrick Racenberg <pracenberg@gmail.com>
2015-11-23 11:09:40 -0800

    options disabled

--- dev/js/mapInfo.js
+++ dev/js/mapInfo.js
@@ -1,15 +1,21 @@
 $('document').ready(function(){
   $.get("/api/map/access", function(data){
     var states = data.map + '?access_token=' + data.token;
+    var mapOptions = {
+      zoomControl: false,
+      minZoom: 4,
+      maxZoom: 4,
+      dragging: false
+    };
     
     //init map and set location
-    var map = createMap('map', [38.925, -98.481], 4);
+    var map = createMap('map', mapOptions, [38.925, -98.481], 4);
     setTile.call(map, states, '<a href="http://mapbox.com">Mapbox</a>');
   });
 }); //end ready
 
-function createMap(id, coords, scale){
-  return L.map(id).setView(coords, scale);
+function createMap(id, options, coords, scale){
+  return L.map(id, options).setView(coords, scale);
 }
 
 
