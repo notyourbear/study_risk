69fffe85e7e0744176eaf2ae69405bda1870e37a
notyourbear <pracenberg@gmail.com>
2018-08-06 21:58:18 -0700

    set up board class prototype

--- dev/models/board.js
+++ dev/models/board.js
@@ -1,10 +1,28 @@
+import states from './states';
+
 /* Board Model:
 * @FIELDS: board field
 */
 
-
 class Board {
   constructor(options) {
+    let { fields } = options;
+
+    this.bordering = {};
+    this.borderStatesColors = ['#fce8c7'];
+    this.currentBorderStatesColor = 0;
+    this.currentUserStateColor = 0;
+    this.enemyStates = {};
+    this.fields = fields || states;
+    this.layers = {};
+    this.stateProps = {};
+    this.userStates = {};
+    this.userStatesColors = ['#b8b453', '#9c9c44', '#96964d', '#8f8e4f'];
+    this.vacantStates = {};
+  }
+
+  init(el) {
+
   }
 }
 
